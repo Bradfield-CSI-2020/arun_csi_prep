fe21a30e9f2548713f996af27fbb624516683b44
Arun Sivasankaran <arunparthiban@gmail.com>
2020-07-08 11:46:02 -0700

    wip

--- 5_the_unix_shell/wiki.sh
+++ 5_the_unix_shell/wiki.sh
@@ -1,23 +1,71 @@
 #!/usr/bin/env bash
-search_term=$1
+title=$1
+section_title=$2
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
 
 # Get search hits
-url="https://en.wikipedia.org/w/api.php?action=opensearch&search=${search_term}&limit=3&namespace=0&format=json"
+url="https://en.wikipedia.org/w/api.php?action=opensearch&search=${title}&limit=3&namespace=0&format=json"
 # echo "$url"
-result=$(curl -s -X GET ${url} | jq -r '.[1] | map(sub(" "; "_")) | join(" ")')
+# result=$(curl -s -X GET ${url} | jq -r '.[1] | map(sub(" "; "_")) | join(" ")')
 
 # arr=($result)
 
-compgen -W "$result"
 # for i in ${arr[@]}; do echo $i; done
-
 # todo: use jq map to replace space with string
 
-
 # get summary
-# url="https://en.wikipedia.org/api/rest_v1/page/summary/${search_term}"
-# echo "$url"
-# curl -X GET ${url} | jq '.extract'
+summary_url="https://en.wikipedia.org/api/rest_v1/page/summary/${title}"
+summary=$(curl -s -X GET "${summary_url}" | jq '.extract')
+
+echo ""
+echo "Summary:"
+echo ""
+echo "${summary}"
+
+
+if [ $# -eq 2 ]
+  then
+    echo ""
+    echo "sections:"
+    echo ""
+    # get sections titles
+    url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${title}&prop=sections&disabletoc=1"
+    response=$(curl -s -X GET ${url})
+    sections=$(echo "${response}" | jq '.parse.sections' | jq '.[] | select(.toclevel == 1) | .line')
+    sections_index=$(echo "${response}" | jq '.parse.sections' | jq '.[] | select(.toclevel == 1) | .number')
+
+    # echo "${sections_index}"
+    # echo "${sections}"
+
+    # hash[${sections_index[i]}]=${sections[i]}
+
+
+    # for i in "${sections_index[@]}"; do 
+    #     echo "${hash[$i]}":; 
+    # done
+
+    section_detail_url="https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${title}&prop=text&section=1&disabletoc=1"
+    response=$(curl -s -X GET "${section_detail_url}")
+    detail=$(echo "${response}" | jq '.parse.text."*"')
+
+    # todo:
+    # the html returned has all the details we need
+    # the section title and the subsection have this before them `class="mw-headline"`
+    # the section details can be found by looking for the first <p>
+    # we can then look for the first ". " ? maybe...
+    # but we need to strip all html in there
+
+    echo "${detail}"
+    
+    # | map(select(.toclevel == 1))
+    # | map(select(.toclevel = 1)) |.line' 
+    exit 0
+fi
 
 
 # get sections titles
@@ -31,6 +79,8 @@ compgen -W "$result"
 # curl -X GET ${section_detail} | jq '.parse.text."*"' > example.html
 
 
+# better section detail 
+# https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Walrus&prop=text&section=3&disabletoc=1
 
 
 
