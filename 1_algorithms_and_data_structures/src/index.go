b9f281706bc263fb60a088c1098f466279a49316
Arun Sivasankaran <arunparthiban@gmail.com>
2020-07-09 23:53:53 -0700

    feat: cleanup main

--- 1_algorithms_and_data_structures/src/index.go
+++ 1_algorithms_and_data_structures/src/index.go
@@ -12,22 +12,7 @@ import (
 
 func main() {
 
-	// Sum of first n numbers
 	fmt.Println("Hello World")
-	fmt.Printf("sumOfFirstN for 10: %v\n", sum.OfFirstN(10))
-	fmt.Printf("sumOfFirstN for 10: %v\n", sum.OfFirstN(100))
-
-
-	// Check 2 strings to see if they are anagrams of each other
-	fmt.Printf("anagramCheck: %v\n", algos.AnagramCheck("hello", "helol"))
-	fmt.Printf("anagramCheck: %v\n", algos.AnagramCheck("bye", "Hello"))
-	// could not run this word in node, the resulting number was too big for nodejs
-	firstWord := "radiometeorograph"
-	wordArray := strings.Split(firstWord, "")
-	sort.Strings(wordArray)
-	newWord := strings.Join(wordArray, "")
-	
-	fmt.Printf("anagramCheck: %v\n", algos.AnagramCheck(firstWord, newWord))
 
 
 	// Create a stack and play with it
@@ -45,8 +30,5 @@ func main() {
 	fmt.Println("item at the top is?" , newStack.Pop())
 	fmt.Println("item at the top is?" , newStack.Pop())
 	fmt.Println("Stack is empty?" , newStack.IsEmpty())	
-
-
-	fmt.Println("Hello?", algos.CheckbalancedParans("{{}}"))
 
 }
