9df92523aff4b5650ecd4c9e1d1301950838712c
Arun Sivasankaran <arunparthiban@gmail.com>
2020-07-09 18:14:05 -0700

    feat: add a stack

---
+++ 1_algorithms_and_data_structures/stack/stack.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var randomStack Stack
+
+	fmt.Println("Stack is empty?" , randomStack.IsEmpty())
+
+	randomStack.Push("Arun")
+	fmt.Println("Stack is empty?" , randomStack.IsEmpty())
+
+	randomStack.Push("Zack")
+	fmt.Println("Stack size is?" , randomStack.Size())
+
+	fmt.Println("item at the top is?" , randomStack.Pop())
+	fmt.Println("item at the top is?" , randomStack.Pop())
+
+	fmt.Println("Stack is empty?" , randomStack.IsEmpty())	
+
+}
+
+// Stack is amazing
+type Stack []string
+
+// IsEmpty returns true only if stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+// Size returns the size of the stack
+func (s *Stack) Size() int {
+	return len(*s)
+}
+
+// Push adds an item to the top of the stack
+func (s *Stack) Push(item string) {
+	*s = append(*s, item)
+}
+
+// Pop returns from the top of the stack
+func (s *Stack) Pop() string {
+	item := (*s)[len(*s) - 1] 
+	*s =  (*s)[:(len(*s) - 1)]
+	return item
+}
