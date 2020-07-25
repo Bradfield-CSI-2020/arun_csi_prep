8bc282d3b746a10eb12f22975b140c02db88e7b1
Arun Sivasankaran <arunparthiban@gmail.com>
2020-07-25 02:26:33 -0700

    feat: use function for temp convertor

--- 3_c_programming_language/temp_convertor.c
+++ 3_c_programming_language/temp_convertor.c
@@ -4,12 +4,12 @@
 #define UPPER 300 /* upper limit of temp */
 #define STEP 20   /* step size */
 
-int main() {
-  float fahr, celsius;
+float fhrToCelsius(float f);
 
-  // use for loop
-  for (fahr = 0.0; fahr <= 300; fahr = fahr + 20) {
-    celsius = 5.0 / 9.0 * (fahr - 32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
+int main() {
+  for (float fahr = 0.0; fahr <= 300; fahr = fahr + 20) {
+    printf("%3.0f %6.1f\n", fahr, fhrToCelsius(fahr));
   }
 }
+
+float fhrToCelsius(float f) { return 5.0 / 9.0 * (f - 32.0); }
