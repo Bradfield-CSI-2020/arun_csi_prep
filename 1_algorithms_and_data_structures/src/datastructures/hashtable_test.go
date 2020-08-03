2d56a41e5187d75eb78ee5bfc3619dff12b369b2
Arun Sivasankaran <arunparthiban@gmail.com>
2020-08-02 17:54:19 -0700

    feat: add a functional has table

--- 1_algorithms_and_data_structures/src/datastructures/hashtable_test.go
+++ 1_algorithms_and_data_structures/src/datastructures/hashtable_test.go
@@ -18,11 +18,36 @@ func TestHashTable(t *testing.T) {
 	newHashTable.Set("d", 4)
 	newHashTable.Set("e", 5)
 
+	got = newHashTable.Get("b")
+
+	if got != 2 {
+		t.Error("expected 2, got ", got)
+	}
+
+	got = newHashTable.Get("c")
+
+	if got != 3 {
+		t.Error("expected 3, got ", got)
+	}
+
 	got = newHashTable.Get("d")
 
 	if got != 4 {
 		t.Error("expected 4, got ", got)
 	}
+
+	got = newHashTable.Get("e")
+
+	if got != 5 {
+		t.Error("expected 5, got ", got)
+	}
+
+	got = newHashTable.Get("f")
+
+	if got != nil {
+		t.Error("expected nil, got ", got)
+	}
+
 }
 
 // todo: set should overwrite an existing key
